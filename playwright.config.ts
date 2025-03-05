79308557ae9a38cb8af6f283953c1075222946c5
mateuszJS <mate.walendzik@gmail.com>
2025-03-06 00:06:02 +0100

    Try to run webkit instead of chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,31 +34,31 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-        launchOptions: {
-          // https://blog.promaton.com/testing-3d-applications-with-playwright-on-gpu-1e9cfc8b54a9
-          args: [
-            "--use-angle=vulkan",
-            "--enable-features=Vulkan",
-            "--disable-vulkan-surface",
-            "--enable-unsafe-webgpu",
-          ]
-        }
-      },
-    },
+    // {
+    //   name: 'chromium',
+    //   use: {
+    //     ...devices['Desktop Chrome'],
+    //     launchOptions: {
+    //       // https://blog.promaton.com/testing-3d-applications-with-playwright-on-gpu-1e9cfc8b54a9
+    //       args: [
+    //         "--use-angle=vulkan",
+    //         "--enable-features=Vulkan",
+    //         "--disable-vulkan-surface",
+    //         "--enable-unsafe-webgpu",
+    //       ]
+    //     }
+    //   },
+    // },
 
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
     // },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
